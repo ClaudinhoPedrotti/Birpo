cfb74c847a3204468e0f8d4c9b8b3ddde45d4176
claudinhopedrotti <claudio.brito9@etec.sp.gov.br>
2019-08-19 21:24:44 -0300

    Estou comitando pq vai que o japa desliga

---
+++ app/src/main/java/br/com/etecia/birpo/ListProdutos.java
@@ -0,0 +1,13 @@
+package br.com.etecia.birpo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListProdutos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_produtos_layout);
+    }
+}
